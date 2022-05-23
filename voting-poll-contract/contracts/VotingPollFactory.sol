24816fb0f7d0efeae8fc5623286670b6790aa31b
charleslukes <charleslukes28@gmail.com>
2022-05-23 13:54:06 +0100

    add voting poll factory

---
+++ voting-poll-contract/contracts/VotingPollFactory.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./VotingPoll.sol";
+
+contract VotingPollFactory {
+    VotingPoll[] public votingPoll;
+    uint256 public numberOfPolls;
+
+    function createPoll(
+        string memory _title,
+        string memory _description,
+        string[] memory _options
+    ) public {
+        VotingPoll newVotingPoll = new VotingPoll(
+            _title,
+            _description,
+            _options
+        );
+        votingPoll.push(newVotingPoll);
+        numberOfPolls = numberOfPolls + 1;
+    }
+
+    function allPolls()
+        public
+        pure
+        returns (VotingPoll[] memory _allVotingPolls)
+    {
+        return _allVotingPolls;
+    }
+}
