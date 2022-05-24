b7bdeeb6709a0659dabe3732023b0fdf4c7fa3bc
charleslukes <charleslukes28@gmail.com>
2022-05-24 06:53:38 +0100

    wrap app in context

--- voting-poll-client/pages/_app.tsx
+++ voting-poll-client/pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { AppWrapper } from "../context";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppWrapper>
+      <Component {...pageProps} />
+    </AppWrapper>
+  );
 }
 
-export default MyApp
+export default MyApp;
