d2d58aab1b68af0a40ed937d4870bb8cbafc0d26
charleslukes <charleslukes28@gmail.com>
2022-05-24 03:41:01 +0100

    update home screen

--- voting-poll-client/pages/index.tsx
+++ voting-poll-client/pages/index.tsx
@@ -1,9 +1,15 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import VotaButton from "../ components/button";
+import PollCard from "../ components/poll-card";
+import WalletConnected from "../ components/wallet-connected";
+import { useAppContext } from "../context";
+import { IPoll } from "../hooks/useWallet";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const context: any = useAppContext();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,56 +23,32 @@ const Home: NextPage = () => {
           Welcome to <a>Vota</a>
         </h1>
 
-        {/* <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p> */}
+        <div className={styles.btnContainer}>
+          {context.isConnected ? (
+            <div>
+              <WalletConnected walletAddress={context.address}></WalletConnected>
+
+            </div>
+          ) : (
+            <VotaButton handleClick={context.connectWallet}></VotaButton>
+          )}
+        </div>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {context.polls.map((data: IPoll, index: number) => (
+            <PollCard
+              key={`${index}-0x`}
+              id={data.id}
+              title={data.title}
+              description={data.description}
+            ></PollCard>
+          ))}
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
